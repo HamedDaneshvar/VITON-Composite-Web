6c182a8ea073d267cb43f18cb03ede8662f4dcd3
Hamed Daneshvar <hmddaneshvar@gmail.com>
2024-09-20 11:57:18 +0330

    feat(web): Add loading icon and handle the logic of web app

--- web_app/app.py
+++ web_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file
+from flask import Flask, render_template, request, url_for, send_from_directory
 import os
-import io
+import uuid
 from PIL import Image
 import torch
 import torchvision.transforms as transforms
@@ -8,6 +8,11 @@ from .models import GMM, SegNet, CompNet
 
 # Define Flask app
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'media'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'}
+
+# Ensure the media directory exists
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 # Load models once on startup
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -19,13 +24,22 @@ comp_net = CompNet().to(device)
 
 # Load saved model weights based on GPU available
 if device == 'cuda':
-    gmm.load_state_dict(torch.load('./model_checkpoints/gmm_epoch_10.pth'))
-    seg_net.load_state_dict(torch.load('./model_checkpoints/seg_net_epoch_10.pth'))
-    comp_net.load_state_dict(torch.load('./model_checkpoints/comp_net_epoch_10.pth'))
+    gmm.load_state_dict(torch.load(
+        './model_checkpoints/gmm_epoch_10.pth'))
+    seg_net.load_state_dict(torch.load(
+        './model_checkpoints/seg_net_epoch_10.pth'))
+    comp_net.load_state_dict(torch.load(
+        './model_checkpoints/comp_net_epoch_10.pth'))
 elif device == 'cpu':
-    gmm.load_state_dict(torch.load('./model_checkpoints/gmm_epoch_10.pth', map_location=torch.device('cpu')))
-    seg_net.load_state_dict(torch.load('./model_checkpoints/seg_net_epoch_10.pth', map_location=torch.device('cpu')))
-    comp_net.load_state_dict(torch.load('./model_checkpoints/comp_net_epoch_10.pth', map_location=torch.device('cpu')))
+    gmm.load_state_dict(torch.load(
+        './model_checkpoints/gmm_epoch_10.pth',
+        map_location=torch.device('cpu')))
+    seg_net.load_state_dict(torch.load(
+        './model_checkpoints/seg_net_epoch_10.pth',
+        map_location=torch.device('cpu')))
+    comp_net.load_state_dict(torch.load(
+        './model_checkpoints/comp_net_epoch_10.pth',
+        map_location=torch.device('cpu')))
 
 # Set models to evaluation mode
 gmm.eval()
@@ -42,7 +56,8 @@ transform = transforms.Compose([
 
 def process_images(body_image_pil, cloth_image_pil):
     """
-    Process the body and cloth images using the loaded models to produce a composite image.
+    Process the body and cloth images using the loaded models to
+    produce a composite image.
     """
     # Preprocess input images
     body_image = transform(body_image_pil).unsqueeze(0).to(device)
@@ -68,6 +83,11 @@ def process_images(body_image_pil, cloth_image_pil):
     return composite_image_pil
 
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in\
+        app.config['ALLOWED_EXTENSIONS']
+
+
 @app.route('/')
 def index():
     """Render the upload form."""
@@ -76,42 +96,58 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    """Handle the upload and redirect to the waiting page."""
+    # Check if files are in the request
     if 'body_image' not in request.files or 'cloth_image' not in request.files:
-        return "Please upload both body and cloth images."
+        return {"error": "Files are missing"}, 400
 
-    # Save uploaded images temporarily
     body_image = request.files['body_image']
     cloth_image = request.files['cloth_image']
 
-    # Save images to static folder
-    body_image.save(os.path.join('static', 'body_image.jpg'))
-    cloth_image.save(os.path.join('static', 'cloth_image.jpg'))
+    # Check if files are allowed
+    if body_image and allowed_file(body_image.filename) and cloth_image and allowed_file(cloth_image.filename):
+        # Generate unique filenames
+        body_image_name = f"{uuid.uuid4()}." + \
+            body_image.filename.rsplit('.', 1)[1].lower()
+        cloth_image_name = f"{uuid.uuid4()}." + \
+            cloth_image.filename.rsplit('.', 1)[1].lower()
 
-    # Redirect to waiting page
-    return redirect(url_for('waiting'))
+        # Save the images
+        body_image_path = os.path.join(app.config['UPLOAD_FOLDER'],
+                                       body_image_name)
+        cloth_image_path = os.path.join(app.config['UPLOAD_FOLDER'],
+                                        cloth_image_name)
+        body_image.save(body_image_path)
+        cloth_image.save(cloth_image_path)
+
+        # Open images with PIL
+        body_image_pil = Image.open(body_image_path)
+        cloth_image_pil = Image.open(cloth_image_path)
+
+        # Process the images
+        processed_image = process_images(body_image_pil, cloth_image_pil)
+
+        # Save processed image
+        processed_image_name = f"{uuid.uuid4()}.png"
+        processed_image_path = os.path.join(app.config['UPLOAD_FOLDER'],
+                                            processed_image_name)
+        processed_image.save(processed_image_path)
+
+        # Redirect to result page
+        return {"redirect": url_for('result',
+                                    filename=processed_image_name)}, 200
+
+    return {"error": "Invalid file type"}, 400
 
 
-@app.route('/waiting')
-def waiting():
-    """Render the waiting page."""
-    # Process images and redirect to result page when done
-    body_image_pil = Image.open(os.path.join('static', 'body_image.jpg')).convert('RGB')
-    cloth_image_pil = Image.open(os.path.join('static', 'cloth_image.jpg')).convert('RGB')
-    composite_image_pil = process_images(body_image_pil, cloth_image_pil)
-
-    # Save the composite image
-    composite_image_pil.save(os.path.join('static', 'composite_image.jpg'))
-
-    # Redirect to the result page
-    return redirect(url_for('result'))
+@app.route('/result/<filename>')
+def result(filename):
+    return render_template('result.html', filename=filename)
 
 
-@app.route('/result')
-def result():
-    """Render the result page with the composite image."""
-    return render_template('result.html', image_url='/static/composite_image.jpg')
+@app.route('/media/<filename>')
+def media(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(port=8000)
